5aa14407b3ff5fa5d3787cf915eeff43bd0f3a15
Catherine Tran <tranc489@gmail.com>
2014-07-21 11:57:53 -0700

    Added var total and quantity to receive total and quantity output.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
+  var total = 0;
+  var quantity = 0;
   $("form#category").submit(function(event) {
     event.preventDefault();
     var description = $("input#description").val();
-    var amount = parseFloat($("input#amount").val()).toFixed(2);
+    var amount = parseInt($("input#amount").val());
     $("#result").show();
     $("ul.purchases").append("<li>" + description + " $" + amount + "</li>");
 
+    total += amount;
+    $(".total").text("$" + total);
 
+    quantity +=1;
+    $(".quantity").text(quantity);
   });
 });
